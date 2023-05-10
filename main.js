d4eed7df7d562859974bf39058466c8c8a4cece3
Priscilha <priscilha@gmail.com>
2023-05-10 19:49:36 -0300

    js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-function fazerClick () {
+const randomNumber = Math.round(Math.random()*10)
+let xAttempts
+
+function actionClick () {
+    event.preventDefault()
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if(Number(inputNumber.value) == randomNumber) {
+        document.querySelector(".screen1").classList.add("hide")
+        document.querySelector(".screen2").classList.remove("hide")
+
+        document.querySelector(".screen2 h2").innerHTML = `Você acertou em ${xAttempts} tentativas`
+
     
+    }
+
+    xAttempts++
+
 }
