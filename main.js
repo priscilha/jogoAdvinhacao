b7982ac3e124c22d1b3e2fd72952faed91d70205
Priscilha <priscilha@gmail.com>
2023-05-16 09:36:08 -0300

    function

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-const randomNumber = Math.round(Math.random()*10)
+
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
 
 //função callback
-function actionTryClick (event) {
+function actionTryClick(event) {
     event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber")
 
-    if(Number(inputNumber.value) == randomNumber) {
-        document.querySelector(".screen1").classList.add("hide")
-        document.querySelector(".screen2").classList.remove("hide")
+    if (Number(inputNumber.value) == randomNumber) {
+        screen1.classList.add("hide")
+        screen2.classList.remove("hide")
 
         document.querySelector(".screen2 h2").innerHTML = `Você acertou em ${xAttempts} tentativas`
 
-    
+
     }
 
     xAttempts++
@@ -28,6 +31,8 @@ const btnReset = document.querySelector("#btnReset")
 
 
 btnTry.addEventListener('click', actionTryClick)
-btnReset.addEventListener('click', function(){
-    
+btnReset.addEventListener('click', function () {
+    screen1.classList.remove("hide")
+    screen2.classList.add("hide")
+
 })
